814629c00f17873d0ed83afe836e5a3f24cb3701
ashutoshiru <ashutoshiruu@gmail.com>
2026-01-28 23:41:43 +0530

    V2.2

--- models/chat.js
+++ models/chat.js
@@ -6,7 +6,7 @@ const chatSchema = new mongoose.Schema({
         required : true
     },
     to : {
-        type : String,
+        type : String, 
         required : true
     },
     message : {
