cbddb06a132167fbc1f1ce042e9096c68d868066
ashutoshiru <ashutoshiruu@gmail.com>
2026-01-29 01:53:11 +0530

    V2.2(adding backup data function)

---
+++ init/index.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Chat = require("../models/chat");
+
+
+main().then(() => console.log("Connection Successful"))
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/Whatsapp');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+
+const initDB = async () => {
+    await Chat.deleteMany({});
+    await Chat.insertMany(initData.data);
+    console.log("DB Initialized with Data");
+}
+
+initDB();
