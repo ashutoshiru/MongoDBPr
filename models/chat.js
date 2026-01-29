155fc0d7557cc2cdef174f97f99d9319070f6570
ashutoshiru <ashutoshiruu@gmail.com>
2026-01-30 00:45:13 +0530

    V2.2(adding backup data function)

--- models/chat.js
+++ models/chat.js
@@ -3,7 +3,7 @@ const mongoose = require ("mongoose");
 const chatSchema = new mongoose.Schema({
     from : {
         type : String,
-        required : true
+        required : true 
     },
     to : {
         type : String, 
