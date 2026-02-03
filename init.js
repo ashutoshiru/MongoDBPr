44db788e1c93bea2c541b1b0e50efff226c69297
Ashutosh kumar <ashutoshiruu@gmail.com>
2026-02-03 23:40:52 +0530

    Update init.js

--- init.js
+++ init.js
@@ -5,7 +5,7 @@ const Chat = require("./models/chat");
 main().then(() => console.log("Connection Successful"))
 .catch(err => console.log(err));
 
-async function main() {
+async function main() { 
   await mongoose.connect('mongodb://127.0.0.1:27017/Whatsapp');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
